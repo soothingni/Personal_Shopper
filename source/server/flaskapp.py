e793b7ac9c9b8d5166f0c41646e2c0f0ac83daae
unknown <sunnydopia@live.co.kr>
2020-04-29 10:07:56 +0900

    폴더 정리

--- source/server/flaskapp.py
+++ source/server/flaskapp.py
@@ -72,5 +72,5 @@ def delete():
 
 
 if __name__ == '__main__':
-    app.run(host='192.168.219.151', port=8484, debug=True)
+    app.run(host='0.0.0.0', port=8484, debug=True)
     # app.run(host='0.0.0.0', port=8484, debug=True)
